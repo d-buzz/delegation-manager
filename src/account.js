3675696c7d075067f214c840508a346472dede6a
Voltair <voltairez@users.noreply.github.com>
2020-08-19 04:28:27 +0800

    validate whether to delegate or remove delegation

--- src/account.js
+++ src/account.js
@@ -1,4 +1,5 @@
 import hiveClient from './hive'
+import dHiveClient from './dhive'
 import axios from 'axios'
 import { SCOT_API_HOST, HIVE_ENGINE_API_HOST } from './config'
 import { parseNumber } from './helper'
@@ -104,4 +105,88 @@ export async function claimRewards(wif, account) {
       resolve('no rewards to claim')
     }
   })
+}
+
+export async function getAccountRC(username) {
+  //capture account
+  const account = await getAccount(username)
+  const props = await dHiveClient.database.getDynamicGlobalProperties()
+  const CURRENT_UNIX_TIMESTAMP = parseInt((new Date(props.time).getTime() / 1000).toFixed(0))
+  //calculate available SP
+  const totalShares = parseFloat(account.vesting_shares) + parseFloat(account.received_vesting_shares) - parseFloat(account.delegated_vesting_shares);
+  //determine elapsed time since last RC update
+  const elapsed = CURRENT_UNIX_TIMESTAMP - account.voting_manabar.last_update_time;
+  const maxMana = totalShares * 1000000;
+  //calculate current mana for the 5 day period (432000 sec = 5 days)
+  const currentMana = parseFloat(account.voting_manabar.current_mana) + elapsed * maxMana / 432000;
+
+  if (currentMana > maxMana) {
+    currentMana = maxMana;
+  }
+  //determine percentage of available mana(RC)
+  // const currentManaPerc = currentMana * 100 / maxMana;
+  return currentMana
+}
+
+export async function delegatablePower(username) {
+  const account = await getAccount(username)
+  const avail = parseFloat(account.vesting_shares) - (parseFloat(account.to_withdraw) - parseFloat(account.withdrawn)) / 1e6 - parseFloat(account.delegated_vesting_shares);
+
+  const props = await dHiveClient.database.getDynamicGlobalProperties();
+  const vestSteem = parseFloat(parseFloat(props.total_vesting_fund_steem) *
+    (parseFloat(avail) / parseFloat(props.total_vesting_shares)), 6);
+  return vestSteem
+}
+
+export async function usablePower(username) {
+  const account = await getAccount(username)
+  const avail = parseFloat(account.vesting_shares) + parseFloat(account.received_vesting_shares);
+
+  const props = await dHiveClient.database.getDynamicGlobalProperties();
+  const vestSteem = parseFloat(parseFloat(props.total_vesting_fund_steem) *
+    (parseFloat(avail) / parseFloat(props.total_vesting_shares)), 6);
+  return vestSteem
+}
+
+export function hasBeneficiarySetting(account, referrer) {
+  let jsonMetadata = account.json_metadata
+  if (jsonMetadata) {
+    jsonMetadata = JSON.parse(jsonMetadata)
+    const beneficiaries = jsonMetadata.beneficiaries
+    if (beneficiaries && beneficiaries.length > 0) {
+      const referred = beneficiaries.filter(b => b.name === referrer)
+      if (referred && referred.length > 0 && referred[0].label === 'referrer') {
+        return true
+      }
+    }
+  }
+  return false
+}
+
+export async function delegatePower(wif, username, receiver, hp) {
+  const account = await getAccount(username)
+  const avail = parseFloat(account.vesting_shares) -
+    (parseFloat(account.to_withdraw) -
+      parseFloat(account.withdrawn)) /
+    1e6 -
+    parseFloat(account.delegated_vesting_shares);
+  const props = await dHiveClient.database.getDynamicGlobalProperties();
+  const vesting_shares = parseFloat(hp * parseFloat(props.total_vesting_shares) / parseFloat(props.total_vesting_fund_steem));
+  if (avail > vesting_shares) {
+    const ops = [[
+      'delegate_vesting_shares',
+      {
+        delegator: username,
+        delegatee: receiver,
+        vesting_shares: Number(vesting_shares).toFixed(6) + ' VESTS'
+      }
+    ]];
+    dHiveClient.broadcast.sendOperations(ops, wif)
+  } else {
+    console.log('no enough Hiver Power for delegation')
+  }
+}
+
+export async function sendMessage(wif, from, to, message) {
+  // transfer
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 // implement the delegation manager according to the description in the post:
-//
+// https://www.sportstalksocial.com/hive-101690/@sportstalksocial/bounty-to-develop-delegation-manager-for-hiveonboard
 
 import config from '../config' // #1 load config
 import { streamOperations, getOperationPerformer } from './operation'
 import { readReferredUsers, saveReferredUsers, addToReferredUsers } from './user'
+import { getMutedAccounts, getAccountRC, delegatablePower, usablePower, getAccount, hasBeneficiarySetting, sendMessage } from './account'
 
 // object keeps the latest referred users
 // user status: { inactive, delegated, muted, expired, beneficiary_removed, graduated }
@@ -29,25 +30,17 @@ function whenReferredUserCreated(op) {
   const name = op.op[0]
   if (['create_claimed_account', 'account_create'].includes(name)) {
     const account = op.op[1]
-    let jsonMetadata = account.json_metadata
-    if (jsonMetadata) {
-      jsonMetadata = JSON.parse(jsonMetadata)
-      const beneficiaries = jsonMetadata.beneficiaries
-      if (beneficiaries && beneficiaries.length > 0) {
-        const referred = beneficiaries.filter(b => b.name === config.delegationAccount)
-        if (referred && referred.length > 0 && referred[0].label === 'referrer') {
-          const username = account.new_account_name
-          console.log('referred user has been created:', username)
-          if (!containsUser(username)) {
-            referredUsers = addToReferredUsers([{
-              account: username,
-              weight: referred[0].weight,
-              timestamp: new Date(op.timestamp + 'Z').getTime()
-            }])
-          } else {
-            console.log(`referred user @${username} already exists. skip.`)
-          }
-        }
+    if (hasBeneficiarySetting(account, config.delegationAccount)) {
+      const username = account.new_account_name
+      console.log('referred user has been created:', username)
+      if (!containsUser(username)) {
+        referredUsers = addToReferredUsers([{
+          account: username,
+          weight: referred[0].weight,
+          timestamp: new Date(op.timestamp + 'Z').getTime()
+        }])
+      } else {
+        console.log(`referred user @${username} already exists. skip.`)
       }
     }
   }
@@ -66,14 +59,57 @@ function whenReferredUserTakeActions(op) {
   }
 }
 
+async function isMuted(username) {
+  const blacklist = await getMutedAccounts(config.muteAccount)
+  return blacklist.includes(username)
+}
+
+async function hasEnoughHP(username) {
+  const hp = await usablePower(username)
+  const maxHP = parseFloat(config.maxUserHP) || 30
+  return hp >= maxHP
+}
+
+async function hasNoRC(username) {
+  const rc = await getAccountRC(username)
+  const commentUnitCost = 1 // TODO: comment transaction average cost
+  const minComments = parseFloat(config.minPostRC) || 2
+  return rc > minComments * commentUnitCost
+}
+
+async function hasSetBeneficiary(username) {
+  const account = await getAccount(username)
+  if (hasBeneficiarySetting(account, config.delegationAccount)) {
+    return true
+  }
+}
+
+async function hasDelegatedTo(username) {
+  return false
+}
+
+async function hasExceedsDelegatinLength(username) {
+  return false
+}
+
+async function notify(receiver, message) {
+  await sendMessage(process.env.ACTIVE_KEY, config.delegationAccount, receiver, message)
+}
+
 async function delegateToUser(username) {
   // delegate to user, when
   // 1. user has little RC
-  // 2. user has low Hive Power (own + received delegation)
-  // 3. user is not muted
-  // 4. we have not delegated to the user before
-  // 5. default beneficiary setting is not removed
-  console.log('delegate to', username)
+  // 2. user has low Hive Power (own + received delegation) < maxUserHP
+  // 3. default beneficiary setting is not removed
+  // 4. user is not muted
+  // 5. we have not delegated to the user before
+  if (!await isMuted(username) && !await hasEnoughHP(username)
+    && await hasNoRC(username) && await hasSetBeneficiary(username)
+    && !await hasDelegatedTo(username))
+  {
+    console.log(`delegate ${config.delegationAmount} HP to @${username}`)
+    await delegatePower(process.env.ACTIVE_KEY, config.delegationAccount, username, parseFloat(config.delegationAmount))
+  }
 
   // #10 if delegation process fail, notify the admin account
 }
@@ -84,13 +120,39 @@ async function removeDelegationIfNeeded(username) {
   // 2. user is muted
   // 3. delegation length exceeds defined cycle
   // 4. default beneficiary setting is removed
-  console.log('remove delegation to', username)
 
-  // #10 if delegation process fail, notify the admin account
+  async function removeDelegation() {
+    await delegatePower(process.env.ACTIVE_KEY, config.delegationAccount, username, 0)
+  }
+
+  if (await hasEnoughHP(username)) {
+    console.log(`remove delegation to @${username}`)
+    await removeDelegation()
+    notify(username, config.delegationMaxMsg)
+  } else if (await isMuted(username)) {
+    console.log(`remove delegation to @${username}`)
+    await removeDelegation()
+    notify(username, config.delegationMuteMsg)
+  } else if (await hasExceedsDelegatinLength(username)) { //
+    console.log(`remove delegation to @${username}`)
+    await removeDelegation()
+    notify(username, config.delegationLengthMsg)
+  } else if (config.beneficiaryRemoval == true && await hasSetBeneficiary(username)) {
+    console.log(`remove delegation to @${username}`)
+    await removeDelegation()
+    notify(username, config.delegationBeneficiaryMsg)
+  }
 }
 
 async function checkDelegatorAccountHP() {
   // #9 if the delegator account has no enough HP, send warning to the admin account
+  const hp = await delegatablePower(config.delegationAccount)
+  const hpWarning = parseFloat(config.hpWarning)
+  if (hpWarning >= 0 && hp < hpWarning) {
+    // send warning
+    console.log('no sufficient HP for delegation')
+    notify(config.adminAccount, config.hpWarningMsg)
+  }
 }
 
 async function processOperations() {
